32b3f0a668c1056254825111add6cc7167134fd0
Eric Davidson <edavidson1410@gmail.com>
2023-05-05 20:41:51 -0500

    game finished. troubleshooting when there is a tie or one of the options is not picked

--- script.js
+++ script.js
@@ -8,25 +8,45 @@ function getComputerChoice() {  //computer picks rock, paper, or scissors
 function oneRound() {
     let computerSelection=getComputerChoice()
     let playerSelection=prompt("Rock, Paper, or Scissors?", "")
+
     playerSelection=playerSelection.toLowerCase()
+
     if (playerSelection=="rock" && computerSelection=="paper" ||
         playerSelection=="paper" && computerSelection=="scissors" ||
         playerSelection=="scissors" && computerSelection=="rock") {
-        return lose
         console.log("Computer picks " + computerSelection + "! You lose!")
+        return "lose"
         }
     else if (playerSelection=="rock" && computerSelection=="scissors" ||
         playerSelection=="paper" && computerSelection=="rock" ||
         playerSelection=="scissors" && computerSelection=="paper") {
-        return win
         console.log("Computer picks " + computerSelection + "! You win!")
+        return "win"
         }
-    else {
+    else if (playerSelection==computerSelection) {  //in case of tie
         console.log("Computer also picked " + computerSelection + "! Go again!")
-        oneRound()
+        return oneRound()
+    }
+    else { //in case of option not in choice array
+        console.log("That's not an option!")
+        return oneRound()
     }
 }
 
 function game() {
-    
+    let number_games=Number(prompt("How many rounds would you like to play?", ""))
+    let player_win=0
+    let player_loss=0
+
+    for (n=0; n<number_games; n++) {  
+        let result=oneRound()
+            if (result=="lose") {    //if player loses
+                player_loss=++player_loss
+            }
+            else if (result=="win") {      //if player wins                              
+                player_win=++player_win
+            }
+        console.log("The score is " + player_win + "-" + player_loss)
+    }
+    console.log("Good Game!")
 }
