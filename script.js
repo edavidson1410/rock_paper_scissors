370d4c78ac1598b166bfb7a238952a61c6d1f0ef
Eric Davidson <edavidson1410@gmail.com>
2023-06-16 19:03:44 -0500

    UI started. JS working. Style to be added.

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
+//beginner scores
+let playerScore = 0;
+let computerScore = 0;
+updateScore();
+
+//randomized computer choice
 function getComputerChoice() {  //computer picks rock, paper, or scissors
-    const choice=["rock", "paper", "scissors"]
-    let list_random=Math.floor(Math.random()*(choice.length)) //picks number between 0 and 1, rounds it down to whole number. multiplies it by length of list.
-    decision=choice.slice(list_random, ++list_random)
-    return decision
+    const choice=["rock", "paper", "scissors"];
+    let list_random=Math.floor(Math.random()*(choice.length)); //picks number between 0 and 1, rounds it down to whole number. multiplies it by length of list.
+    decision=choice.slice(list_random, ++list_random);
+    return decision;
 }
 
-function oneRound() {
-    let computerSelection=getComputerChoice()
-    let playerSelection=prompt("Rock, Paper, or Scissors?", "")
+//play a round
+function oneRound(choice) {
+    let computerSelection=getComputerChoice();
+    let playerSelection = choice;
 
-    playerSelection=playerSelection.toLowerCase()
 
     if (playerSelection=="rock" && computerSelection=="paper" ||
         playerSelection=="paper" && computerSelection=="scissors" ||
         playerSelection=="scissors" && computerSelection=="rock") {
-        console.log("Computer picks " + computerSelection + "! You lose!")
-        return "lose"
+        computerScore++
+        return `Computer picks ${computerSelection}! You lose.`;
         }
     else if (playerSelection=="rock" && computerSelection=="scissors" ||
         playerSelection=="paper" && computerSelection=="rock" ||
         playerSelection=="scissors" && computerSelection=="paper") {
-        console.log("Computer picks " + computerSelection + "! You win!")
-        return "win"
+        playerScore++
+        return `Computer picks ${computerSelection}! You win!`;
         }
     else if (playerSelection==computerSelection) {  //in case of tie
-        console.log("Computer also picked " + computerSelection + "! Go again!")
-        return oneRound()
+        return `Computer picks ${computerSelection}! It's a tie!`;
     }
-    else { //in case of option not in choice array
-        console.log("That's not an option!")
-        return oneRound()
-    }
+    updateScore()
 }
 
-function game() {
-    let number_games=Number(prompt("How many rounds would you like to play?", ""))
-    let player_win=0
-    let player_loss=0
+//updates score on html
+function updateScore() {
+    const playerScoreDiv = document.getElementById("playerScore");
+    const computerScoreDiv = document.getElementById("computerScore");
 
-    for (n=0; n<number_games; n++) {  
-        let result=oneRound()
-            if (result=="lose") {    //if player loses
-                player_loss=++player_loss
-            }
-            else if (result=="win") {      //if player wins                              
-                player_win=++player_win
-            }
-        console.log("The score is " + player_win + "-" + player_loss)
-    }
-    console.log("Good Game!")
+    playerScoreDiv.textContent = `Player: ${playerScore}`;
+    computerScoreDiv.textContent = `Computer: ${computerScore}`;
 }
+
+//plays game on click of choice and updates score.
+const choiceBtns = document.querySelectorAll(".btn");
+choiceBtns.forEach(choice => choice.addEventListener("click", () => {
+    document.getElementById("gameResult").innerHTML = oneRound(choice.id);
+    updateScore();
+}))
+
+//plays game on click of choice button
+// const btns = document.querySelectorAll(".btn");
+// for (let i = 0; i < btns.length; i++) {
+//     btns[i].onclick = function() {
+//         document.getElementById("gameResult").innerHTML = oneRound(btns[i].id);
+//   }
+// }
+
+
+
+
+//OUTDATED FUNCTION FOR UI
+// function game(choice) {
+//     let player_win=0;
+//     let player_loss=0;
+
+//     for (n=0; n < 5; n++) {  
+//         let result=oneRound(choice);
+//             if (result=="lose") {    //if player loses
+//                 player_loss=++player_loss;
+//             }
+//             else if (result=="win") {      //if player wins                              
+//                 player_win=++player_win;
+//             }
+//         console.log("The score is " + player_win + "-" + player_loss);
+//     }
+//     console.log("Good Game!");
+// }
+
+
